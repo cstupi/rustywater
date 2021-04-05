156a3688ebcc84aa1823caae558a9b689d5205c3
clay <clay.stup@gmail.com>
2021-04-05 04:55:19 +0000

    Change endpoints to use pin and use toggle and time

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,35 @@
 #[macro_use] extern crate rocket;
 
 use gpio::{GpioOut};
+use std::thread::spawn;
+use std::thread::sleep;
+use std::time::Duration;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[get("/pump/<pin>/<enable>")]
-fn pump(pin: u16, enable: bool) {
+#[get("/<pin>/toggle/<enable>")]
+fn toggle(pin: u16, enable: bool) {
     let mut gpio = gpio::sysfs::SysFsGpioOutput::open(pin).unwrap();
     gpio.set_value(enable).expect("could not set gpio4");
+}
+
+#[get("/<pin>/timed/<time>")]
+fn timed(pin: u16, time: u64) {
+    spawn(move || {
+        let mut gpio = gpio::sysfs::SysFsGpioOutput::open(pin).unwrap();
+        gpio.set_value(true).expect("could not set pin");
+        sleep(Duration::from_secs(time));
+        gpio.set_value(false).expect("could not set pin");
+        println!("Pin: {}, Time: {}", pin, time);
+    });
 }
 
 fn main() {
     rocket::ignite()
     .mount("/", routes![index])
-    .mount("/", routes![pump])
+    .mount("/pin", routes![toggle, timed])
     .launch();
 }
