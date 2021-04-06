0d1b6638d4e143ce0b017cdfac96bb84aa9c6d47
clay <clay.stup@gmail.com>
2021-04-06 03:42:38 +0000

    add blink. use put to set status

--- README.md
+++ README.md
@@ -1,12 +1,7 @@
 # rustywater
 
-Very simple api to interface with gpio on raspberry pi.
-
-Currently just triggers on and off with an endpoint.
-
+Very simple web api to interface with gpio on raspberry pi.
 
 TODO:
 GET to retrieve values
-PUT to set out value
-Timer based triggering
-Blink
+Validation
--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,25 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[get("/<pin>/toggle/<enable>")]
+#[put("/<pin>/toggle/<enable>")]
 fn toggle(pin: u16, enable: bool) {
     let mut gpio = gpio::sysfs::SysFsGpioOutput::open(pin).unwrap();
     gpio.set_value(enable).expect("could not set gpio4");
 }
 
+#[put("/<pin>/blink/<interval>/count/<count>")]
+fn blink(pin: u16, interval: u64, count: u64) {
+    spawn(move || {
+        let mut gpio = gpio::sysfs::SysFsGpioOutput::open(pin).unwrap();
+        for _i in 1..count {
+            gpio.set_value(true).expect("could not set pin");
+            sleep(Duration::from_secs(interval));
+            gpio.set_value(false).expect("could not set pin");
+        }
+    });
+}
+
+// Webhook compatible way to turn on a pin for a time
 #[get("/<pin>/timed/<time>")]
 fn timed(pin: u16, time: u64) {
     spawn(move || {
@@ -28,6 +41,8 @@ fn timed(pin: u16, time: u64) {
         println!("Pin: {}, Time: {}", pin, time);
     });
 }
+
+
 
 fn main() {
     rocket::ignite()
